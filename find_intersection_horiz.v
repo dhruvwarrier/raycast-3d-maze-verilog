aa9f20527880377c3e21839b074ea82fc99bb9d3
Dhruv Warrier <dhruv.warrier89@gmail.com>
2018-11-10 21:40:14 -0500

    Added prototype for find_intersection_horiz module's datapath - Control inputs and outputs of datapath are the same as outputs and inputs of FSM Data input and output: - playerX (input) - playerY (input) - alpha (input, angle of ray currently being cast) - currentX (output, current X pos of ray, is = wallX if reached_wall) - currentY (output, current Y pos of ray, is = wallY if reached_wall)

--- find_intersection_horiz.v
+++ find_intersection_horiz.v
@@ -7,9 +7,34 @@ module find_wall_intersection_horiz
 		input clock, 										// On board clock, 50 MHz for the DE1_SoC
 		input resetn, 										// active-low, resets the FSM and clears the datapath registers
 		input begin_calc,									// begins calculation of wall intersection
-		output [11:0] wall_X, wall_Y,					// calculated wall X and Y for this ray
-		output wall_found									// high if wall is found, low if not
+		output [11:0] wallX, wallY,					// calculated wall X and Y for this ray
+		output wall_found,								// high if wall is found, low if not
+		output end_calc									// calculation has ended, whether wall found or not
 	);
+	
+	// tells the datapath to calculate the first intersection of the ray with the grid
+	wire find_first_intersection;
+	
+	// tells the datapath to calculate the X and Y offset to find new intersections of the ray with the grid
+	wire find_offset;
+	
+	// tells the datapath to find the next intersection of the ray with the grid
+	wire find_next_intersection;
+	
+	// tells the control that the first intersection was found
+	wire found_first_intersection;
+	
+	// tells the control the X and Y offsets were found
+	wire found_XY_offset;
+	
+	// if either reached_wall or reached_maze_bounds, start at the beginning and wait for begin_calc again
+	wire reached_wall, reached_maze_bounds;
+	
+	// wall_found is always reached_wall, if no wall is reached then end_calc is high but wall_found is 0
+	assign wall_found = reached_wall;
+	
+	// high for one cycle when either wall is reached or bounds are reached
+	assign end_calc = reached_wall || reached_maze_bounds;
 
 	control FSM(
 	
@@ -22,6 +47,7 @@ module find_wall_intersection_horiz
 		
 		.found_first_intersection(found_first_intersection),
 		.found_XY_offset(found_XY_offset),
+		// if either reached_wall or reached_maze_bounds, go back to beginning
 		.reached_wall(reached_wall),
 		.reached_maze_bounds(reached_maze_bounds),
 		
@@ -38,6 +64,33 @@ module find_wall_intersection_horiz
 	);
 	
 	datapath position_manip(
+	
+		.clock(clock),
+		.resetn(resetn),
+		
+		// ------------------------------------ control signals from FSM --------------------------------------
+		
+		.find_first_intersection(find_first_intersection),
+		.find_offset(find_offset),
+		.find_next_intersection(find_next_intersection),
+		
+		// ------------------------------------ data input and output --------------------------------------
+		
+		.playerX(playerX),
+		.playerY(playerY),
+		.alpha(alpha),
+		
+		// when reached_wall is high, currentX = wallX, currentY = wallY
+		.currentX(wallX),
+		.currentY(wallY),
+		
+		// ----------------------------------------- outputs to FSM -----------------------------------------
+		// ----------------------- inform the FSM when calculations are complete --------------------------------
+		
+		.found_first_intersection(found_first_intersection),
+		.found_XY_offset(found_XY_offset),
+		.reached_wall(reached_wall),
+		.reached_maze_bounds(reached_maze_bounds)
 	
 	);
 
