525d2b5cb053b8961e4872542f0ee904f2a7c698
Dhruv Warrier <dhruv.warrier89@gmail.com>
2018-11-18 15:13:05 -0500

    Removed last remaining since Modelsim doesn't compile it

--- fixed_point_arithmetic.v
+++ fixed_point_arithmetic.v
@@ -10,6 +10,7 @@ module int_fixed_point_mult_int
 
 	// here fixed_Y must be the binary representation of the integer to the right of the decimal point
 	// accuracy of fixed point value is assumed to be 5 d.p. so divide by 10^5
+	
 	assign int_out = int_in * fixed_X + ((int_in * fixed_Y) / 100000);
 
 endmodule
@@ -26,15 +27,15 @@ module int_fixed_point_div_int
 	begin
 	
 		if (fixed_X == 10'b0 && fixed_Y != 18'b0)
-			int_out <= $rtoi((int_in / fixed_Y) * 100000);
+			int_out <= (int_in / fixed_Y) * 100000;
 		else if (fixed_X != 10'b0 && fixed_Y == 18'b0)
 			int_out <= int_in / fixed_X;
 		else if (fixed_X == 10'b0 && fixed_Y == 18'b0)
 			int_out <= 21'b011111111111111111111; // incredibly high positive value
 		else
 			int_out <= int_in / fixed_X + ((int_in / fixed_Y) * 100000);
-	
-	end 
+
+	end
 
 endmodule
 
