187550fe06097ab393343595e522275d1c6c8aa2
Dhruv Warrier <dhruv.warrier89@gmail.com>
2018-11-24 20:33:42 -0500

    Completed implementation of control_find_slice_size - States of this FSM implemented according to control signals described in previous commit. - Each state moves immediately to the next state at the next clock cycle, with the exception of S_WAIT (moves ahead at high begin_calc) and S_RAYCAST, which moves ahead at high end_calc_raycast, sent by datapath when raycast calculations are done - Todo: correct S_RAYCAST logic, should go back to S_WAIT, send out high end_calc and slice_size = 0 if no wall found

--- find_slice_size.v
+++ find_slice_size.v
@@ -114,3 +114,91 @@ module find_slice_size
 	);
 	
 endmodule
+
+module control_find_slice_size (input clock, resetn, begin_calc, end_calc_raycast,
+										  output reg find_angle_offset, find_alpha_beta_0, find_alpha_beta_1,
+										  find_ray_grid_intersections, find_distances_0, find_distances_1, find_closer_distance,
+										  perform_reverse_fishbowl, perform_project_to_screen);
+	
+	reg [3:0] current_state, next_state;
+	
+	localparam S_WAIT = 4'd0,
+				  S_FIND_ANGLE_OFFSET = 4'd1,
+				  S_FIND_ALPHA_BETA_0 = 4'd2,
+				  S_FIND_ALPHA_BETA_1 = 4'd3,
+				  S_RAYCAST = 4'd4,
+				  S_FIND_DISTANCES_0 = 4'd5,
+				  S_FIND_DISTANCES_1 = 4'd6,
+				  S_FIND_CLOSER_DIST = 4'd7,
+				  S_REVERSE_FISHBOWL = 4'd8,
+				  S_PROJECT_TO_SCREEN = 4'd9;
+				  
+	// ----------------------------------------- state table  ------------------------------------------------
+	
+	always @(*)
+	begin: state_table
+	
+		case(current_state)
+			S_WAIT: next_state = begin_calc ? S_FIND_ANGLE_OFFSET : S_WAIT;
+			S_FIND_ANGLE_OFFSET: next_state = S_FIND_ALPHA_BETA_0; // provide 1 state to compute angle offset
+			S_FIND_ALPHA_BETA_0: next_state = S_FIND_ALPHA_BETA_1; // provide 2 states to compute alpha and beta
+			S_FIND_ALPHA_BETA_1: next_state = S_RAYCAST;
+			S_RAYCAST: next_state = end_calc_raycast ? S_FIND_DISTANCES_0 : S_RAYCAST; // stay in this state till raycasts are complete
+			S_FIND_DISTANCES_0: next_state = S_FIND_DISTANCES_1; // provide 2 states to compute distances from horizontal and vertical raycasts
+			S_FIND_DISTANCES_1: next_state = S_FIND_CLOSER_DIST;
+			S_FIND_CLOSER_DIST: next_state = S_REVERSE_FISHBOWL; // provide 1 state to find lesser of 2 distances
+			S_REVERSE_FISHBOWL: next_state = S_PROJECT_TO_SCREEN; // provide 1 state to reverse fishbowl effect
+			S_PROJECT_TO_SCREEN: next_state = S_WAIT;
+	
+	end // state_table
+	
+	// ------------------------------- output logic i.e. control signal logic  -------------------------------------
+	
+	always @(*)
+	begin: control_signals
+
+		// prevent latching by assuming all control signals to be 0 at the beginning
+		find_angle_offset = 1'b0;
+		find_alpha_beta_0 = 1'b0;
+		find_alpha_beta_1 = 1'b0;
+		find_ray_grid_intersections = 1'b0;
+		find_distances_0 = 1'b0;
+		find_distances_1 = 1'b0;
+		find_closer_distance = 1'b0;
+		perform_reverse_fishbowl = 1'b0;
+		perform_project_to_screen = 1'b0;
+		
+		case(current_state)
+			S_FIND_ANGLE_OFFSET: find_angle_offset = 1'b1;
+			S_FIND_ALPHA_BETA_0: find_alpha_beta_0 = 1'b1;
+			S_FIND_ALPHA_BETA_1: find_alpha_beta_1 = 1'b1;
+			S_RAYCAST: find_ray_grid_intersections = 1'b1;
+			S_FIND_DISTANCES_0: find_distances_0 = 1'b1;
+			S_FIND_DISTANCES_1: find_distances_1 = 1'b1;
+			S_FIND_CLOSER_DIST: find_closer_distance = 1'b1;
+			S_REVERSE_FISHBOWL: perform_reverse_fishbowl = 1'b1;
+			S_PROJECT_TO_SCREEN: perform_project_to_screen = 1'b1;
+		endcase
+		
+	end // control_signals
+	
+	// ------------------------------------- current state register  -------------------------------------------
+	
+	always @(posedge clock)
+	begin: state_FFs
+		if (!resetn)
+			current_state <= S_WAIT;
+		else
+			current_state <= next_state; // at each clock cycle, move to the next computed state
+	end // state_FFs
+										  
+endmodule
+
+module datapath_find_slice_size (input clock, resetn, find_angle_offset, find_alpha_beta_0, find_alpha_beta_1,
+										  find_ray_grid_intersections, find_distances_0, find_distances_1, find_closer_distance,
+										  perform_reverse_fishbowl, perform_project_to_screen,
+										  input signed [12:0] playerX, playerY, input signed [9:0] angle_X, angle_Y,
+										  input [7:0] column_count,
+										  output [6:0] proj_slice_size, output end_calc_raycast);
+										  
+endmodule
