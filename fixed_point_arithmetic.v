1ccc090572170e235b75d7e6caf33e79800388e6
ayesha2199 <44934280+ayesha2199@users.noreply.github.com>
2018-11-25 23:17:04 -0500

    Update fixed_point_arithmetic.v

--- fixed_point_arithmetic.v
+++ fixed_point_arithmetic.v
@@ -86,6 +86,10 @@ module fixed_point_subtract_fixed_point
 				fixed_X_out <= ((fixed_X_in_1) - fixed_X_in_2);
 				fixed_Y_out <= ((fixed_Y_in_2) - fixed_Y_in_1);
 			end
+			else if (fixed_X_in_1 == fixed_X_in_2) begin
+				fixed_X_out <= 512;
+				fixed_Y_out <= (fixed_Y_in_2 - fixed_Y_in_1);
+			end
 			else begin
 				fixed_X_out <= ((fixed_X_in_1-1) - fixed_X_in_2);
 				fixed_Y_out <= (( 1000- fixed_Y_in_2) + fixed_Y_in_1);
